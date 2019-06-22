58bbe80cf4454bca82a2c0ffa65e1641837f275e
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2019-06-21 20:34:37 -0500

    remove unneeded functions from touch

--- src/touch.c
+++ src/touch.c
@@ -11,13 +11,6 @@ extern short vxoff,yoff;
 extern short work_out[57];
 
 /**
- * touch_init() - Set up touch screen
- */
-void touch_init(void)
-{
-}
-
-/**
  * touch_main() - Main loop for touch screen
  * mx - mouse X from multi()
  * my - mouse Y from multi()
@@ -42,26 +35,5 @@ void touch_main(short mx, short my)
  * touch_allow - Set whether touchpanel is active or not.
  */
 void touch_allow(padBool allow)
-{
-}
-
-/**
- * handle_mouse - Process mouse events and turn into scaled touch events
- */
-void handle_mouse(void)
-{
-}
-
-/**
- * touch_hide() - hide the mouse cursor
- */
-void touch_hide(void)
-{
-}
-
-/**
- * touch_done() - Stop the mouse driver
- */
-void touch_done(void)
 {
 }
